b0fe755bdb64c781dc0f945b646412d72487ab39
Bari <alex.c9777700@gmail.com>
2020-10-19 15:19:40 +0200

    Add mob and player life points

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    /// <summary>
+    /// Property to store player life points
+    /// </summary>
+    public int life = 10;
+
+    /// <summary>
+    /// Method called when Player object is loaded
+    /// </summary>
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Method to trigger when enemy hit
+    /// </summary>
+    /// <param name="other"></param>
+    void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.CompareTag("Mob"))
+        {
+            var mob = other.GetComponent<BasicMob>();
+            if (mob != null)
+            {
+                GetDamage(mob);
+            }
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    /// <summary>
+    /// Get damage from mob
+    /// </summary>
+    /// <param name="mob">Mob hited by</param>
+    private void GetDamage(BasicMob mob)
+    {
+        life -= mob.damage;
     }
 }
