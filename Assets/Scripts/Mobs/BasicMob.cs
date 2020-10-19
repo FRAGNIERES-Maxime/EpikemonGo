7bf5f4d2dcfd868f2985e6f79670d6494377a36e
Bari <alex.c9777700@gmail.com>
2020-10-19 14:30:22 +0200

    Add mob and colision

---
+++ Assets/Scripts/Mobs/BasicMob.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMob : MonoBehaviour
+{
+    public float speed;
+
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
