bd0b88870683b463f8b3adc067e1ccd4770e1788
Maxime-Frag <fragnieresmaxime@gmail.com>
2021-01-08 17:06:11 +0100

    Rajout son, mort mob stable

--- Assets/Scripts/BulletPlayer.cs
+++ Assets/Scripts/BulletPlayer.cs
@@ -9,6 +9,8 @@ public class BulletPlayer : MonoBehaviour
     public GameObject EndPointLightning;
     public GameObject Lightning;
     private bool canDamage = false;
+    private bool currentDamage = false;
+    private bool colorChange = false;
     private GameObject CurrentMob;
 
     // Start is called before the first frame update
@@ -23,7 +25,7 @@ public class BulletPlayer : MonoBehaviour
         RaycastHit hit;
         int layerMask = 1 << 8;
         layerMask = ~layerMask;
-        Ray ray = camera.ViewportPointToRay(new Vector3(0, 0, 200f));
+        Ray ray = new Ray(camera.transform.position, camera.transform.forward * 100);
 
         if (Physics.Raycast(ray, out hit))
         {
@@ -33,11 +35,18 @@ public class BulletPlayer : MonoBehaviour
                 CurrentMob = hit.transform.gameObject;
                 canDamage = true;
                 EndPointLightning.gameObject.transform.position = hit.transform.gameObject.transform.position;
+                if (currentDamage == false)
+                {
+                    StartCoroutine(TouchMob());
+                    if (colorChange == false)
+                        StartCoroutine(ChangeColor(hit.transform.gameObject));
+                }
                 Lightning.gameObject.SetActive(true);
             }
         }
         else
         {
+            currentDamage = false;
             canDamage = false;
             CurrentMob = null;
             Lightning.gameObject.SetActive(false);
@@ -46,11 +55,35 @@ public class BulletPlayer : MonoBehaviour
 
     IEnumerator TouchMob()
     {
+        currentDamage = true;
+        GetComponent<AudioSource>().Play();
         while (canDamage)
         {
             CurrentMob.gameObject.GetComponent<MobBehaviour>().LoseLife(10);
-            yield return new WaitForSeconds(0.1f);
+            yield return new WaitForSeconds(0.5f);
         }
+        GetComponent<AudioSource>().Stop();
+        yield return null;
+    }
+
+    IEnumerator ChangeColor(GameObject Mob)
+    {
+        colorChange = true;
+        float startTime = Time.time;
+        Renderer[] allChildren = Mob.GetComponentsInChildren<Renderer>();
+        float T = Time.time * 1.0f;
+        foreach(Renderer child in allChildren)
+        {
+            T = (startTime - Time.time) * 1.0f;
+            child.material.color = Color.Lerp(Color.white, Color.red, T);
+        }
+        //yield return new WaitForSeconds(0.2f);
+        foreach(Renderer child in allChildren)
+        {
+            T = (Mathf.Sin(startTime - Time.time) * 1.0f);
+            child.material.color = Color.Lerp(Color.red, Color.white, T);
+        }
+        colorChange = false;
         yield return null;
     }
 }
